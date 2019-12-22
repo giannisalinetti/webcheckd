c5fda389da154b993d86286088f06310a7ab70a9
Gianni Salinetti <gbsalinetti@extraordy.com>
2019-12-22 01:40:59 +0100

    First working email notification

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM golang:latest
+LABEL maintainer="Giovan Battista Salinetti <gbsalinetti@gmail.com>"
+
+ENV URL=https://www.paradewedding.com \
+    FROM=sender@gmail.com \
+    PASSWORD=mypassword \
+    TO=recipient@gmail.com
+
+# Define workdir
+WORKDIR /site-checker
+
+# Copy appliation files and dependencies
+COPY go.mod go.sum main.go ./
+
+# Downlaod modules and build
+RUN go mod download && \
+    go build
+
+CMD ./site-checker -url $URL -from $FROM -password $PASSWORD -to $TO
--- main.go
+++ main.go
@@ -2,34 +2,98 @@ package main
 
 import (
 	"flag"
-	"log"
 	"net/http"
+	"net/smtp"
 	"regexp"
+	"time"
+
+	log "github.com/sirupsen/logrus"
 )
 
+const recipientsSize = 8
+
+// sliceFlag defines a new type to hold multiple recipients
+type sliceFlag []string
+
+// String implements the stringer interface
+func (s *sliceFlag) String() string {
+	return ("Implementation of the String interface")
+}
+
+// Set adds new entries to the slice
+func (r *sliceFlag) Set(value string) error {
+	*r = append(*r, value)
+	return nil
+}
+
+// smtpServer configuration struct
+type smtpServer struct {
+	host string
+	port string
+}
+
+// serverName URI to smtp server
+func (s *smtpServer) Address() string {
+	return s.host + ":" + s.port
+}
+
+// mailSender sends email notification messages in case of errors
+func mailSender(from string, password string, to []string, message []byte) {
+
+	smtpServerInstance := &smtpServer{host: "smtp.gmail.com", port: "587"}
+
+	// Authentication
+	auth := smtp.PlainAuth("", from, password, smtpServerInstance.host)
+
+	// Sending email
+	err := smtp.SendMail(smtpServerInstance.Address(), auth, from, to, message)
+	if err != nil {
+		log.Errorf("Error sending email notification", err)
+		return
+	}
+
+	log.Info("Notification e-mail sent.")
+}
+
+// siteChecker verifies the status of the site
 func siteChecker(url string) (bool, string) {
 	r, err := regexp.Compile(`^200 OK$`)
 	if err != nil {
-		log.Fatal("FATAL: Unable to compile the regexp")
+		log.Fatal("Unable to compile the regexp.")
 	}
 	client := &http.Client{}
 	rsp, err := client.Get(url)
 	if err != nil {
-		log.Printf("ERR: %v\n", err)
+		log.Errorf("%v\n", err)
 	}
 
 	return r.MatchString(rsp.Status), rsp.Status
 }
 
 func main() {
+	var recipientsList sliceFlag
+	flag.Var(&recipientsList, "to", "Recipient e-mail")
 	siteUrl := flag.String("url", "http://www.example.com", "Url name")
+	senderAccount := flag.String("from", "sender@gmail.com", "Sender account")
+	senderPassword := flag.String("password", "mypassword", "Sender password")
+
 	flag.Parse()
 
-	ok, status := siteChecker(*siteUrl)
+	for {
+		ok, status := siteChecker(*siteUrl)
 
-	if ok {
-		log.Printf("INFO: %s is up. Status: %s\n", *siteUrl, status)
-	} else {
-		log.Printf("INFO: %s is down. Status: %s\n", *siteUrl, status)
+		mailMessage := []byte("Subject: Site status notification.\r\n" +
+			"\r\n" +
+			"This is an alert message for " + *siteUrl + ". Status is down with error " + status + "\r\n")
+
+		if ok {
+			log.Infof("%s is up. Status: %s\n", *siteUrl, status)
+			//mailMessage := fmt.Sprintf("ALERT: The site" + *siteUrl + "is down!")
+		} else {
+			log.Warnf("%s is down. Status: %s\n", *siteUrl, status)
+			//mailMessage := fmt.Sprintf("ALERT: The site" + *siteUrl + "is down!")
+			mailSender(*senderAccount, *senderPassword, recipientsList, mailMessage)
+		}
+		time.Sleep(300 * time.Second)
 	}
 }
