e088d1da96ce4bbb0949fd69e32f1c3894ed384c
Gianni Salinetti <gbsalinetti@extraordy.com>
2019-12-22 02:24:56 +0100

    Add logrus formatting and a minimal http health check

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
 	"net/smtp"
 	"regexp"
@@ -37,6 +38,7 @@ func (s *smtpServer) Address() string {
 	return s.host + ":" + s.port
 }
 
+
 // mailSender sends email notification messages in case of errors
 func mailSender(from string, password string, to []string, message []byte) {
 
@@ -66,19 +68,36 @@ func siteChecker(url string) (bool, string) {
 	if err != nil {
 		log.Errorf("%v\n", err)
 	}
-
 	return r.MatchString(rsp.Status), rsp.Status
 }
 
+// healthCheck returns a status string for the site-checker service
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status OK.\n")
+}
+
 func main() {
+	// Set logrus custom formatter
+	customFormatter := new(log.TextFormatter)
+    customFormatter.TimestampFormat = "2006-01-02 15:04:05"
+	log.SetFormatter(customFormatter)
+	customFormatter.FullTimestamp = true
+	
+	// Set commandline flags
 	var recipientsList sliceFlag
 	flag.Var(&recipientsList, "to", "Recipient e-mail")
 	siteUrl := flag.String("url", "http://www.example.com", "Url name")
 	senderAccount := flag.String("from", "sender@gmail.com", "Sender account")
 	senderPassword := flag.String("password", "mypassword", "Sender password")
-
 	flag.Parse()
 
+	// Start embedded web server for service health probes
+	go func() {
+		http.HandleFunc("/healthz", healthCheck)
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	}()
+
+	// Start a deferred closure for site check
 	defer func() {
 		for {
 			ok, status := siteChecker(*siteUrl)
@@ -100,5 +119,4 @@ func main() {
 	}()
 
 	log.Infof("Check loop for %s started.", *siteUrl)
-
 }
